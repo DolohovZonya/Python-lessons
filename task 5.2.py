508fcf1378ebb240ebc556fba680728d554ff69a
DolohovZonya <114208871+DolohovZonya@users.noreply.github.com>
2022-10-24 00:26:20 +0700

    Update task 5.2.py

--- task 5.2.py
+++ task 5.2.py
@@ -1,8 +1,11 @@
-from timeit import default_timer
+from timeit import default_timer as dt
 import random as rd
-x = 2
+import numpy as np
+x = 3
 for i in range(1, x+1):
   b = 2**i
+  k = 0
+  k2 = 0
   matrix = [[0]*b for i in range(b)]
   matrix2 = [[0]*b for i in range(b)]
   new_matrix = [[0]*b for i in range(b)]
@@ -10,8 +13,14 @@ for i in range(1, x+1):
     for j in range(len(matrix)):
       matrix[w][j] = rd.randint(0, 9)
       matrix2[w][j] = rd.randint(0, 9)
-      start = default_timer()
+      start = dt()
       for i in range(101):
         new_matrix[w][j] = matrix[w][j]*matrix2[w][j]
-      duration = default_timer() - start
-  print(matrix, matrix2, new_matrix, duration)
+      duration = dt() - start
+      k += duration
+      start2 = dt()
+      for i in range(101):
+       m_numpy = np.dot(matrix, matrix2)
+      duration2 = dt() - start2
+      k2 += duration2
+  print(new_matrix, '\n', m_numpy, '\n', duration, '\n', duration2)
