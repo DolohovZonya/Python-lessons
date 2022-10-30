e337b685f44307c93905ce5b00181dbce2aaa4bc
DolohovZonya <114208871+DolohovZonya@users.noreply.github.com>
2022-10-30 23:23:41 +0700

    Update task 6.2.py

--- task 6.2.py
+++ task 6.2.py
@@ -1,35 +1,37 @@
 import pandas as pd
 
-def concensus(string):
-  cons = list(string)
+df = pd.read_excel("input.6.2.xlsx")
+string = df['sequence']
+cons = list(string)
+print(type(string))
+print(type(cons[1]))
+for i in range(len(cons)):
   num_a = 0
   num_t = 0
   num_c = 0
   num_g = 0
   max_num = []
-  for i in range(len(string)):
-    if cons[i] == 'A':
-      num_a += 1
-    if cons[i] == 'T':
-      num_t += 1
-    if cons[i] == 'C':
-      num_c += 1
-    if cons[i] == 'G':
-      num_g += 1
+  if cons[i] == 'A':
+    num_a += 1
+  if cons[i] == 'T':
+    num_t += 1
+  if cons[i] == 'C':
+    num_c += 1
+  if cons[i] == 'G':
+    num_g += 1
   max_num.append(num_a)
   max_num.append(num_g)
   max_num.append(num_t)
   max_num.append(num_c)
   max_number = max(max_num)
+  print(max_num)
   if max_number == num_a:
-    return 'A'
+    print('A')
   if max_number == num_t:
-    return 'T'
+    print('T')
   if max_number == num_c:
-    return 'C'
+    print('C')
   if max_number == num_g:
-    return 'G'
-df = pd.read_excel("input.6.2.xlsx")
-df["sequence"].apply(concensus)
+    print('G')
 
 print(df)
