50abd2f4646ef38e3a62dd03e52558a7b11f9604
DolohovZonya <114208871+DolohovZonya@users.noreply.github.com>
2022-10-31 12:00:29 +0700

    Create task 8.3.py

---
+++ task 8.3.py
@@ -0,0 +1,59 @@
+from Bio import pairwise2
+def compare(sym1, sym2):
+  if sym1 == sym2:
+    return 1
+  else:
+    return -1
+
+def NW(x, y, g):
+  lx = len(x)
+  ly = len(y)
+  matrix = [[0]*(lx+1) for i in range(ly+1)]
+  print(*matrix, sep='\n')
+  for i in range(lx+1):
+    matrix[0][i] = g*i
+  for j in range(ly+1):
+    matrix[j][0] = g*j
+  for i in range(1, ly+1):
+    for j in range(1, lx):
+      diag = matrix[i-1][j-1] + compare(x[j-1], y[i-1])
+      up = matrix[i-1][j] + g
+      left = matrix[i][j-1] + g
+      matrix[i][j] = max(diag, up, left)
+  print(*matrix, sep='\n')
+  #обратный ход
+  i = ly
+  j = lx 
+  alig1 = []
+  alig2 = []
+  while (i != 0) or (j != 0):
+    diag = matrix[j-1][i-1] + compare(x[j-1], y[i-1])
+    up = matrix[i-1][j] + g
+    left = matrix[i][j-1] + g
+    if matrix[i][j] == diag:
+      i = i-1
+      j = j-1
+      alig1.append(x[j])
+      alig2.append(y[i])
+    if matrix[i][j] == up:
+      i = i-1
+      alig1.append('-')
+      alig2.append(y[i])
+    else:
+      j = j-1
+      alig2.append('-')
+      alig1.append(x[j])     
+  while i != 0:
+    i = i-1
+    alig1.append('-')
+    alig2.append(y[i])
+  while j != 0:
+    j = j-1
+    alig1.append(x[j])
+    alig2.append('-')
+  return matrix[-1][-1], ''.join(alig1[::-1]), ''.join(alig2[::-1])
+  aligments = pairwise2.align.globalms(x, y, 1, -1, -1, -1)
+s1 = 'ATGC'
+s2 = 'TTG'
+gap = -1
+print(NW(s1, s2, gap))
