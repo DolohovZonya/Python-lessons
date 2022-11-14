9c5ef65495d4ef9ca31af8e61e38f1bc2a64d69c
DolohovZonya <114208871+DolohovZonya@users.noreply.github.com>
2022-11-14 19:26:56 +0700

    Update task 7.2.py

--- task 7.2.py
+++ task 7.2.py
@@ -2,30 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def consensus(df, string):
-    for i in columns:
-      a = dict(df[i].value_counts())
-      x = max(a, key = a.get)
-      string += x
-    return string
+def maximum(df, columns):
+  codes = {'A': 'A', 'C': 'C', 'G': 'G', 'T': 'T', 
+    'GA': 'R', 'TC': 'Y', 'GC': 'S', 'TA': 'W', 
+    'TG': 'K', 'CA': 'M', 'GCT': 'B', 'TGC': 'B',
+    'CTG': 'B', 'GTC': 'B', 'TCG': 'B', 'ATC': 'D', 
+    'TCA': 'D', 'TAC': 'D', 'CAT': 'D', 'CTA': 'D', 
+    'AGC': 'V', 'GAC': 'V', 'GCA': 'V', 'CAG': 'V',
+    'CGA': 'V', 'AG': 'R', 'CT': 'Y', 'CG': 'S', 'AT': 'W', 
+    'GT': 'K', 'AC': 'M', 'CGT': 'B', 'AGT': 'D', 
+    'ACT': 'H', 'ACG': 'V', 'ACGT': 'N', '\n': ''}
+  string = ''
+  for i in columns:
+    df_dict = dict(df[i].value_counts())
+    keys = ''
+    max = 0
+    for k, v in df_dict.items():
+      if v < max:
+        continue
+      if v > max:
+        max = v
+      if v == max:
+        keys += k
+    for k in codes.keys():
+      if keys == k:
+        string += codes[k]
+  print(string)
 file_name = input()
 dna1 = []
 dna3 = []
-string = ''
 with open(file_name, "r") as buf:
   dna = buf.readlines()
 for i in range(len(dna)):
   dna1.append(list(dna[i]))
-columns = [int(i) for i in range(len(dna[0]))]
+columnss = [int(i) for i in range(len(dna[0]))]
 index = [int(i) for i in range(len(dna))]
-print(columns)
-df = pd.DataFrame(dna1, index, columns)
-consensus(df, string)
-for i in columns:
-  a = dict(df[i].value_counts())
-  x = max(a, key = a.get)
-  string += x
-print(string)
+df = pd.DataFrame(dna1, index, columnss)
+maximum(df, columnss)
 for seq in dna1:
   dna2 = []
   num_a = seq.count('A')
@@ -41,7 +54,6 @@ print(dna3)
 x = np.arange(len(dna3))
 fig, ax = plt.subplots()
 for i in range(len(dna3)):
-  print(dna3[i][0])
   ax.bar(x, dna3[i][0])
   ax.bar(x, dna3[i][1], bottom=dna3[i][0])
   ax.bar(x, dna3[i][2], bottom=dna3[i][1])
