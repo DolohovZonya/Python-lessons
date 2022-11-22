67bbee6d0484eeebe86d43b2340e98d7bb25d535
DolohovZonya <114208871+DolohovZonya@users.noreply.github.com>
2022-11-23 01:36:22 +0700

    Update task 6.2.py

--- task 6.2.py
+++ task 6.2.py
@@ -2,36 +2,14 @@ import pandas as pd
 
 df = pd.read_excel("input.6.2.xlsx")
 string = df['sequence']
-cons = list(string)
-print(type(string))
-print(type(cons[1]))
-for i in range(len(cons)):
-  num_a = 0
-  num_t = 0
-  num_c = 0
-  num_g = 0
-  max_num = []
-  if cons[i] == 'A':
-    num_a += 1
-  if cons[i] == 'T':
-    num_t += 1
-  if cons[i] == 'C':
-    num_c += 1
-  if cons[i] == 'G':
-    num_g += 1
-  max_num.append(num_a)
-  max_num.append(num_g)
-  max_num.append(num_t)
-  max_num.append(num_c)
-  max_number = max(max_num)
-  print(max_num)
-  if max_number == num_a:
-    print('A')
-  if max_number == num_t:
-    print('T')
-  if max_number == num_c:
-    print('C')
-  if max_number == num_g:
-    print('G')
-
-print(df)
+big = []
+consensus = ''
+for pd in string:
+  l_pd = list(pd)
+  big.append(l_pd)
+  new_list = list(map(list, zip(*big)))
+for i in range(len(l_pd)):
+  df[f'pos{i}'] = new_list[i]
+  df_max = df[f'pos{i}'].value_counts().idxmax()
+  consensus += df_max
+print(consensus)
