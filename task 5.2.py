03e2676dab3cd74567f94eefe982b798d52258ac
DolohovZonya <114208871+DolohovZonya@users.noreply.github.com>
2022-12-12 23:58:45 +0700

    Update task 5.2.py

--- task 5.2.py
+++ task 5.2.py
@@ -4,16 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 labels = ['2*2', '4*4', '8*8', '16*16', '32*32', '64*64', '128*128']
-for i in range(2):
+h_t = []
+np_t = []
+h_end = []
+np_end = []
+np_st = []
+h_st = []
+numpy_d = {}
+hand_d = {}
+for i in range(2,9):
   b = 2**i
-  h_t = []
-  np_t = []
-  h_end = []
-  np_end = []
-  np_st = []
-  h_st = []
-  numpy_d = {}
-  hand_d = {}
   matrix = [[0]*b for i in range(b)]
   matrix2 = [[0]*b for i in range(b)]
   init_m = [[0]*b for i in range(b)]
@@ -23,7 +23,7 @@ for i in range(2):
       matrix[w][j] = rd.randint(0, 9)
       matrix2[w][j] = rd.randint(0, 9)
   start = dt()
-  for i in range(30000):
+  for i in range(3):
     for f in range(len(matrix)):
       for x in range(len(matrix)):
         for y in range(len(matrix)):
@@ -34,33 +34,32 @@ for i in range(2):
   h_end.append(np.mean(h_t))
   hand_d[b] = np.mean(h_t)
   h_st.append(np.std(h_t))
-  fig1 = plt.figure(2, figsize=(12,8))
-  gridForm = (1,1)
-  ay = plt.subplot2grid(gridForm, (0,0))
-  ay.bar(labels, hand_d.values())
-  ay.set_title("hand time")
-  ay.set_xlabel("matrix size")
-  ay.set_ylabel("time")
-  ay.set_xticklabels(labels)
-  plt.yscale('log')
-  plt.show()
   start2 = dt()
-  for i in range(30000):
+  for i in range(3):
     m_numpy = np.dot(matrix, matrix2)
   duration2 = dt() - start2
   np_t.append(duration2)
   np_end.append(np.mean(np_t))
   numpy_d[b] = np.mean(np_t)
   np_st.append(np.std(np_t))
-  fig2 = plt.figure(1, figsize=(12,8))
-  gridForm = (1,1)
-  ax = plt.subplot2grid(gridForm, (0,0))
-  ax.bar(labels, numpy_d.values())
-  ax.set_title("numpy time")
-  ax.set_xlabel("matrix size")
-  ax.set_ylabel("time")
-  ax.set_xticklabels(labels)
-  plt.yscale('log')
-  plt.show()
-  print('no numpy: ', h_t, 'numpy: ', np_t)
-  # print(new_matrix, '\n', m_numpy, '\n', duration, '\n', duration2)
+print('no numpy: ', h_t, 'numpy: ', np_t)
+fig1 = plt.figure(2, figsize=(12,8))
+gridForm = (1,1)
+ay = plt.subplot2grid(gridForm, (0,0))
+ay.bar(labels, hand_d.values())
+ay.set_title("hand time")
+ay.set_xlabel("matrix size")
+ay.set_ylabel("time")
+ay.set_xticklabels(labels)
+plt.yscale('log')
+plt.show()
+fig2 = plt.figure(1, figsize=(12,8))
+gridForm = (1,1)
+ax = plt.subplot2grid(gridForm, (0,0))
+ax.bar(labels, numpy_d.values())
+ax.set_title("numpy time")
+ax.set_xlabel("matrix size")
+ax.set_ylabel("time")
+ax.set_xticklabels(labels)
+plt.yscale('log')
+plt.show()
