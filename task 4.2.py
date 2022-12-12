85b365b7485010362894dc183c565f6143bbd5e5
DolohovZonya <114208871+DolohovZonya@users.noreply.github.com>
2022-12-12 15:11:03 +0700

    Update task 4.2.py

--- task 4.2.py
+++ task 4.2.py
@@ -18,68 +18,69 @@ geneticCode = {'TTT':'F', 'TTC':'F', 'TCT':'S', 'TCC':'S',
                'GAA':'E', 'GAG':'E', 'GGA':'G', 'GGG':'G'}
 
 class orf:
-  def __init__(self, seq, frame, geneticCode):
+  def __init__(self, seq, seq1, frame, geneticCode):
     self.seq = seq
     self.frame = int(frame)
     self.geneticCode = geneticCode
     self.stt = []
-    self.start = 0
-    self.stop = 0
+    self.att = []
     self.translation = ''
-    self.a = 0
-    self.b = 0
+    self.stop = []
+    self.start = []
+    self.seq1 = seq1
   def find_orf(self):
-    #выбор рамки считывания
-    for i in range(int(self.frame), len(self.seq)-2,3):
-      self.stt.append(str(self.seq[i:i+3]))
-    try:
-      self.start += self.stt.index("ATG")*3
-    except:
-      self.start = -1
-    try:
-      self.stop += self.stt.index("TGA")*3 or self.stt.index("TAG")*3 or self.stt.index("TAA")*3
-    except:
-      self.stop = -1
-    print(self.start, self.stop)
-    print(self.stt)
-    #определение условий трансляции
-    while self.a != -1 and self.b != -1:
-      if self.stop > self.start:
-        for i in range(int(self.start/3), int(self.stop/3)):
-          for k, v in self.geneticCode.items():
-            if k == self.stt[i]:
-                self.translation += str(v)
-            self.a += str(self.stt).find('ATG')
-            if self.a != -1:
-              self.start = self.a
-            self.b += str(self.stt).find('TAG') or str(self.stt).find('TGA') or str(self.stt).find('TAA')
-            if self.b != -1:
-                self.stop = self.b
-        for i in range(int(self.stop/3), len(self.stt)):
-          for k, v in self.geneticCode.items():
-              if k == self.stt[i]:
-                  self.translation += str(v)
-              self.a += str(self.stt).find('ATG')
-              if self.a != -1:
-                self.start = self.a
-              self.b += str(self.stt).find('TAG') or str(self.stt).find('TGA') or str(self.stt).find('TAA')
-              if self.b != -1:
-                  self.stop = self.b
-        if self.stop < self.start:
-          for i in range(0, int(self.stop/3)):
-            for k, v in self.geneticCode.items():
-              if k == self.stt[i]:
-                  self.translation += str(v)
-          self.a += str(self.stt).find('ATG')
-          if self.a != -1:
-            self.start = self.a
-          self.b += str(self.stt).find('TAG') or str(self.stt).find('TGA') or str(self.stt).find('TAA')
-          if self.b != -1:
-              self.stop = self.b
-      print(str(self.translation))
-      print(self.start, self.stop)
-frame = input() 
-for record in SeqIO.parse('input.4.2 - Copy.txt', 'fasta'):
+    self.seq = self.seq[self.frame:]
+    stop_codons = ['TGA', 'TAG', 'TAA']
+    sc, start = 0, 0
+    for i in range(0, len(self.seq), 3):
+      if self.seq[i:i+3] in stop_codons:
+        self.stt.append(str(self.seq[sc:i+3]))
+        sc, start = i+3, 0
+        self.stop.append(i)
+      elif self.seq[i:i+3] == 'ATG':
+        sc = i
+        start = 1
+        self.start.append(sc)
+      if start == 1 and (i+3 == len(self.seq)):
+        self.stt.append(str(self.seq[sc:i+3]))
+    if self.stt == []:
+      self.stt.append(str(self.seq))
+    for subseq in self.stt:
+      for i in range(0,len(subseq), 3):
+        for k, v in self.geneticCode.items():
+          if subseq[i:i+3] == k and str(v):
+            self.translation += str(v)
+      self.att.append(self.translation)
+      self.translation = ''
+    for s in self.att:
+      if s == 'None':
+        self.att.remove(s)
+    for i in self.stop:
+      if i == 0:
+        self.stop.remove(i)
+    file_out = open("output.4.2.txt", "r")
+    file_out.close()
+    file_out = open("output.4.2.txt", "a")
+    for i in range(len(self.att)):
+      sub3 = str(self.att[i])
+      sub2 = sub3.replace('None', '')
+      if sub3[0] == 'M' and sub3[len(sub3)-4:len(sub3)] == 'None':
+        string = sub2 + '|' + seq1 + '|' + 'complete' + '|' + str(self.start[i]) + '|' + str(self.stop[i]) + '\n'
+        file_out.write(string)
+      if sub3[0] == 'M' and sub3[len(sub3)-4:len(sub3)] != 'None':
+        string = sub2 + '|' + seq1 + '|' + 'start' + '|' + str(self.start[i]) + '\n'
+        file_out.write(string)
+      if sub3[0] != 'M' and sub3[len(sub3)-4:len(sub3)] == 'None':
+        string = sub2 + '|' + seq1 + '|' + 'stop' + '|' + str(self.stop[i]) + '\n'
+        file_out.write(string)
+      if sub3[0] != 'M' and sub3[len(sub3)-4:len(sub3)] != 'None':
+        string = sub2 + '|' + seq1 + '|' + 'empty' + '\n'
+        file_out.write(string)
+    file_out.close()
+#ввод рамки считывания
+frame = input()
+for record in SeqIO.parse('input.4.2.txt', 'fasta'):
   seq = record.seq
-  trans = orf(seq, frame, geneticCode)
+  seq1 = record.id
+  trans = orf(seq, seq1, frame, geneticCode)
   trans.find_orf()
